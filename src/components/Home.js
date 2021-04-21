d202bcaa1774c3f8eeae8a65dd817f9574ffade8
Marwa64 <farabi.marwa@gmail.com>
2021-04-21 22:14:02 +0200

    no tasks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,10 @@
 import Button from './Button';
 import Tasks from './Tasks';
-import NoTasks from './NoTasks';
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 const Home = () => {
-  const [tasks, setTasks] = useState([
-    {
-      "id": 1,
-      "name": "Doctor's Appointment"
-    },
-    {
-      "id": 2,
-      "name": "AOS Assignment"
-    }
-  ]);
+  const [tasks, setTasks] = useState([]);
 
 
   const green = '#21AC0F', grey = '#787878';
@@ -26,7 +16,7 @@ const Home = () => {
         <Button text='Remove All' color={grey} padding='20px'/>
       </div>
       <div style={{marginTop: '140px', padding: '25px'}}>
-        {tasks.length > 0 ? <Tasks tasks={tasks}/> : <NoTasks />}
+        {tasks.length > 0 ? <Tasks tasks={tasks}/> : <h2> Your to-do list is empty, add a new task now! </h2>}
       </div>
     </>
   );
